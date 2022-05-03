7ad82b86159946fce596f3364315abee8febbed6
Devang Manjramkar <devangmanjramkar@gmail.com>
2022-05-03 10:42:03 +0530

    Finished with model training

--- model.py
+++ model.py
@@ -1,12 +1,38 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+import joblib
 
-data = pd.read_csv('dataset.csv')
+data = pd.read_csv('train.csv')
 
 le = LabelEncoder()
 data['Gender'] = le.fit_transform(data['Gender'])
 input_cols = ['Gender', 'Age', 'openness', 'neuroticism',
               'conscientiousness', 'agreeableness', 'extraversion']
 output_cols = ['Personality (Class label)']
+
+scaler = StandardScaler()
+data[input_cols] = scaler.fit_transform(data[input_cols])
+data.head()
+
+X = data[input_cols]
+Y = data[output_cols]
+
+model = LogisticRegression(multi_class='multinomial',
+                           solver='newton-cg', max_iter=1000)
+model.fit(X, Y.values.ravel())
+
+test_data = pd.read_csv('test.csv')
+test_data['Gender'] = le.fit_transform(test_data['Gender'])
+test_data[input_cols] = scaler.fit_transform(test_data[input_cols])
+X_test = test_data[input_cols]
+Y_test = test_data['Personality (class label)']
+test_data.head()
+
+y_pred = model.predict(X_test)
+
+print(accuracy_score(Y_test, y_pred)*100)
+
+joblib.dump(model, "train_model.pkl")
