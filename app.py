96ad968c070eff8709b71bb2ec615cbeb39d6e1d
Devang Manjramkar <devangmanjramkar@gmail.com>
2022-06-09 22:33:47 +0530

    heroku fixes --more

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+import pathlib
+
 import numpy as np
 from flask import Flask, render_template, request
 from sklearn.preprocessing import StandardScaler
 import joblib
 app = Flask(__name__)
-model = joblib.load("train_model.pkl")
+import os
+cwd = os.getcwd()
+mod = pathlib.Path('train_model.pkl')
+model = joblib.load(mod)
 scaler = StandardScaler()
 
 
@@ -23,10 +28,7 @@ def result():
         extraversion = request.form['extraversion']
         female = request.form['isFemale']
         male = request.form['isMale']
-        result = np.array([age, openness, neuroticism,
-                          conscientiousness, agreeableness, extraversion, female, male], ndmin=2)
-        final = scaler.fit_transform(result)
-        personality = str(model.predict(final))
+        personality = str(model.predict([[int(age),int(openness),int(neuroticism),int(conscientiousness),int(agreeableness),int(extraversion),int(female),int(male)]]))
         return render_template("submit.html", answer=personality.replace('[', '').replace(']', '').replace("'", ""))
 
 
